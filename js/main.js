d46c0999c69c7ca51ce7e95ca84e1924d082fc77
Amit <90555965+amitjimiwal@users.noreply.github.com>
2022-12-19 23:26:14 +0530

    temp branch commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,63 @@
 const pfp=document.getElementById('pfp');
-const name=document.querySelector('.name');
+const pname=document.querySelector('.pname');
 const username=document.querySelector('.username');
 const following=document.getElementById('following');
 const followers=document.getElementById('followers');
 const profilelink=document.querySelector('.button a');
-
 const input=document.getElementById('userid');
-
+const loader=document.querySelector('.loader');
 const form=document.getElementById('inputform');
 
+
+const hideload=()=>{
+   loader.style.display='none';
+}
+
+const showload=()=>{
+      loader.style.display='block';
+}
+
 const fetchdata= usr=>{
-      fetch('https://api.github.com/users/amitjimiwal')
+      fetch(`https://api.github.com/users/${usr}`)
       .then(response=> response.json())
       .then(data=>{
-            
-      });
+            if(data.login==undefined){
+                  hideuser();   
+                  alert(`Can't Find the User.`);              
+                  return;
+            }
+            else{
+                  pfp.src=data.avatar_url;
+                  pname.innerText=data.name; 
+                  username.innerText=usr;       
+                  followers.innerText=`Following: ${data.followers}`;
+                  following.innerText=`Following: ${data.following}`;
+                  profilelink.innerHTML='Profile';
+                  profilelink.href=data.html_url;
+            }
+      });  
 }
-fetchdata('amit')
+
+function showuser(){
+      document.getElementById('userinfo').style.display='block';
+}
+function hideuser(){
+      document.getElementById('userinfo').style.display='none';
+}
+
+form.addEventListener('submit',(e)=>{
+      e.preventDefault();
+      hideuser();
+      showload();
+      setTimeout(()=>{
+            hideload();
+            showuser();
+            fetchdata(input.value);
+      },3000);
+});
+
+
+
+
 
 
